54aa722c5470bbb215b610dc7a6dafb014256936
vmccaffrey <vmccaffrey@troy.edu>
2018-03-20 22:39:58 -0500

    Added database files along with the queries used to make the database

---
+++ Database/MySQL/Readme.txt
@@ -0,0 +1,5 @@
+The file webdev.sql is a single file containing the entire database
+
+The Table folder contains .sql files for each table in the database
+
+They're the same thing, so load either the single file or the whole Table folder
---
+++ Database/Queries/Query_Create_Forums.sql
@@ -0,0 +1,11 @@
+insert into forum (forumTopic, forumName) values
+	('Hardware', 'CPUs'),
+	('Hardware', 'CPU Coolers'),
+	('Hardware', 'Motherboards'),
+	('Hardware', 'Memory'),
+	('Hardware', 'Storage'),
+	('Hardware', 'Video Cards'),
+	('Hardware', 'Power Supplies'),
+	('Hardware', 'Cases'),
+	('General', 'System Builds'),
+	('General', 'General Discussion');
