d60314354230f4e683fb1a24625abb11a01e87f7
vmccaffrey <vmccaffrey@troy.edu>
2018-04-04 22:04:51 -0500

    Web Scraper Update

---
+++ Database/Web Scraper/network_functions.py
@@ -0,0 +1,56 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from lxml.html import fromstring
+
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None
+    """
+    try:
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)'
+                                ' Chrome/41.0.2228.0 Safari/537.36'}
+        with closing(get(url, stream=True, headers=header)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns true if the response seems to be HTML, false otherwise
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors.
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:20]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            # Grabbing IP and corresponding PORT
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
--- Database/Web Scraper/scraper.py
+++ Database/Web Scraper/scraper.py
@@ -1,145 +1,68 @@
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
 from bs4 import BeautifulSoup
-from pathlib import Path
-import os
-import errno
+import network_functions as net
+import directory_functions as dirf
+
+proxy_list = net.get_proxies()
+for i in proxy_list:
+    print(i)
+
+print(dirf.get_project_root())
+
+# cpu_link = simple_get('http://www.cpu-world.com/CPUs/CPU.html')
+# cpu_link = simple_get('http://www.cpu-world.com/CPUs/Celeron_D/index.html')
+# cpu_link = simple_get('http://www.cpu-world.com/CPUs/Celeron_D/TYPE-Celeron%20D%20(Prescott).html')
+# cpu_soup = BeautifulSoup(cpu_link, 'html.parser')
+file_root = open('Local HTML\\root.html', 'r')
+file_family_list = open('Local HTML\\family_list.html', 'r')
+file_cpu_list = open('Local HTML\\cpu_list.html', 'r')
+file_cpu = open('Local HTML\\cpu.html', 'r')
+cpu_soup = BeautifulSoup(file_root, 'html.parser')
+
+# Find list of cpu families manufactured in or after 2003
+for table in cpu_soup.find_all('div', class_='p_div'):
+    if table.find('tr').find('th').contents[0] == 'AMD':
+        print(table.find('tr').find('th').contents[0])
+        for row in table.find_all('tr')[2:]:
+            year = ''.join(c for c in row.find_all('td')[1].contents[0] if c.isdigit())
+            if len(year) < 4:
+                year += '3'
+            if int(year) >= 2003:
+                print(row.find('a').get('href') + '\n' + year)
+        print()
+
+    if table.find('tr').find('th').contents[0] == 'Intel':
+        print(table.find('tr').find('th').contents[0])
+        for row in table.find_all('tr')[2:]:
+            year = ''.join(c for c in row.find_all('td')[1].contents[0] if c.isdigit())
+            if len(year) < 4:
+                year += '3'
+            if int(year) >= 2003:
+                print(row.find('a').get('href') + '\n' + year)
+
+cpu_soup = BeautifulSoup(file_family_list, 'html.parser')
+
+print()
+
+# Get links to processors in each family
+for family in cpu_soup.find_all('div', class_='cpu_name'):
+    print('Link: ' + family.find('a').get('href'))
+    print('Family: ' + family.find('a').contents[0])
+
+cpu_soup = BeautifulSoup(file_cpu_list, 'html.parser')
+
+print()
+
+# Follow links to each processor
+for family in cpu_soup.find_all('div', class_='cpu_name'):
+    print('Link: ' + family.find('a').get('href'))
+    print('CPU: ' + family.find('a').contents[0])
+
+cpu_soup = BeautifulSoup(file_cpu, 'html.parser')
+
+print()
+
+# Pull data from each processor
 
 
-def simple_get(url):
-    """
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None
-    """
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None
 
 
-def is_good_response(resp):
-    """
-    Returns true if the response seems to be HTML, false otherwise
-    """
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200
-            and content_type is not None
-            and content_type.find('html') > -1)
-
-
-def log_error(e):
-    """
-    It is always a good idea to log errors.
-    This function just prints them, but you can
-    make it do anything.
-    """
-    print(e)
-
-
-# directory_this = os.path.dirname(os.path.abspath(__file__))
-#
-# directory_intel = os.path.join(directory_this, 'CPU_Intel\\')
-# directory_intel_families = os.path.join(directory_intel + 'Families\\')
-# directory_intel_families_cpu = os.path.join(directory_intel_families, 'CPU\\')
-#
-# directory_AMD = os.path.join(directory_this, 'CPU_AMD\\')
-# directory_AMD_families = os.path.join(directory_AMD + 'Families\\')
-# directory_AMD_families_cpu = os.path.join(directory_AMD_families, 'CPU\\')
-
-# try:
-#     os.makedirs(directory_intel_families_cpu)
-# except OSError as e:
-#     if e.errno != errno.EEXIST:
-#         raise
-#
-# try:
-#     os.makedirs(directory_AMD_families_cpu)
-# except OSError as e:
-#     if e.errno != errno.EEXIST:
-#         raise
-#
-# hyperlink_list = []
-# hyperlink_list_all_cpu = []
-#
-# intel_html = simple_get('https://ark.intel.com/')
-# intel_cpu = BeautifulSoup(intel_html, 'html.parser')
-#
-# AMD_html = simple_get('AMD')
-# AMD_cpu = BeautifulSoup(AMD_html, 'html.parser')
-
-# for div in intel_cpu.find_all('div', class_="products processors"):
-#     for link in div.find_all('a'):
-#         hyperlink_list.append('https://ark.intel.com' + link.get('href'))
-#
-# for link in hyperlink_list:
-#     print(link)
-#
-#
-# file_series = os.path.join(directory_intel, 'cpu_families_list.txt')
-#
-# file = open(file_series, 'w')
-# for link in hyperlink_list:
-#     file.write(link + '\n')
-# file.close()
-#
-# for link in hyperlink_list:
-#     temp_link = BeautifulSoup(simple_get(link), 'html.parser')
-#     split_array = link.split('/')
-#     print(split_array[-1])
-#     for row in temp_link.find_all('tbody'):
-#         cpu_hyperlinks = []
-#         for entry in row.find_all('a'):
-#             cpu_hyperlinks.append('https://ark.intel.com' + entry.get('href'))
-#             hyperlink_list_all_cpu.append('https://ark.intel.com' + entry.get('href'))
-#         temp_file = Path(os.path.join(directory_intel_families, split_array[-1] + '.txt'))
-#         file = open(temp_file, 'w')
-#         for cpu_link in cpu_hyperlinks:
-#             file.write(cpu_link + '\n')
-#         file.close()
-
-
-intel_link = simple_get('https://ark.intel.com/products/130409/Intel-Core-i7-8809G-Processor'
-                        '-with-Radeon-RX-Vega-M-GH-graphics-8M-Cache-up-to-4_20-GHz')
-soup = BeautifulSoup(intel_link, 'html.parser')
-
-# Model
-for thing in soup.find_all('div', id="tab-blade-1-4"):
-    other_thing = thing.find('ul', class_="specs-list").find('li').find_next_sibling().find('span', class_="value").contents
-    if other_thing is None:
-        print('Oh shit')
-    else:
-        print(other_thing)
-# Type
-# Brand
-# Series
-for thing in soup.find_all('li', class_="ParentGroup"):
-    print(thing.find('span', class_="value").find('a').contents)
-# Name
-for thing in soup.find_all('li', class_="ProcessorNumber"):
-    print(thing.find('span', class_="value").find('span').contents)
-# Socket
-for thing in soup.find_all('li', class_="SocketsSupported"):
-    print(thing.find('span', class_="value").find('span').contents)
-# Core Name
-for thing in soup.find_all('li', class_="CodeNameText"):
-    print(str(thing.find('span', class_="value").find('a').contents).replace('Products formerly ', ''))
-# Core Count
-for thing in soup.find_all('li', class_="CoreCount"):
-    print(thing.find('span', class_="value").find('span').contents)
-# Thread Count
-for thing in soup.find_all('li', class_="ThreadCount"):
-    print(thing.find('span', class_="value").find('span').contents)
-# Speed
-for thing in soup.find_all('li', class_="ClockSpeed"):
-    print(thing.find('span', class_="value").find('span').contents)
-# TDP
-for thing in soup.find_all('li', class_="MaxTDP"):
-    print(thing.find('span', class_="value").find('span').contents)
